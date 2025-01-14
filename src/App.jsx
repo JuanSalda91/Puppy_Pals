17971565e2c74098b884d3a4f32ac7ddf5f32908
jpsalda6@hotmail.com <sejuan$@SamsungJS.>
2025-01-13 19:00:27 -0500

    updated jxs

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,41 @@ import './App.css';
 
 function App() {
 const [puppies, setPuppies] = useState(puppyList);
+const [featPupId, setFeatPupId] = useState(null);
 
-console.log("puppyList: ", puppyList);
+
+//click function//
+function handleClick(puppyId) {
+console.log("Puppy ID", puppyId);
+setFeatPupId(puppyId);
+}
+
+//fin puppy with matching ID//
+const featuredPup = puppies.find((pup) => pup.id ===featPupId)
+console.log(featuredPup)
 
 return (
     <div className="App">
-      {
-      puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>
-      })
-      }
+      {puppies.map((puppy) => {
+        return (
+        <p
+        onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+        );
+      })}
+      
+      {featPupId && ( 
+        <div> 
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
+    //rendering list of puppies info//
  );
 }
 
